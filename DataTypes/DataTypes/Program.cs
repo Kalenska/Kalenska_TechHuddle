a1460e9a238f4b4f85329f5f04bb1948e563fb81
Kalenska <julianakalenska@gmail.com>
2020-02-24 13:02:13 +0200

    data types homework

--- DataTypes/DataTypes/Program.cs
+++ DataTypes/DataTypes/Program.cs
@@ -11,16 +11,15 @@ namespace DataTypes
             string city = "Sofia";
             string street = "Bul. Bulgaria";
             string houseNumber = "N5";
-            string name = string.Concat(firstName, lastName);
+            int age = 20;
+            string university = "Sofia University";
 
-  
-            Console.WriteLine("Name: " + firstName + " " + lastName + "," + " Address: " + city + ", "  + street + " " + houseNumber);
+            //Problem 1 
+            Console.WriteLine("Name: " + firstName + " " + lastName + ", " + "Address: " + city + ", " + street + " " + houseNumber);
 
-           
-           
-
-
-            //
+            //Problem 2
+            Console.WriteLine("Name: " + firstName + " " + lastName + ", " + "Age: " + age);
+            Console.WriteLine("University: " + university);
 
 
         }
