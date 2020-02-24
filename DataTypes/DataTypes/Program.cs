bf843993ca853ddd04c98863cbdfb202ae2915c7
Kalenska <julianakalenska@gmail.com>
2020-02-24 12:47:10 +0200

    Data types homework

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Ivan";
+            string lastName = "Petkov";
+            string city = "Sofia";
+            string street = "Bul. Bulgaria";
+            string houseNumber = "N5";
+            string name = string.Concat(firstName, lastName);
+
+  
+            Console.WriteLine("Name: " + firstName + " " + lastName + "," + " Address: " + city + ", "  + street + " " + houseNumber);
+
+           
+           
+
+
+            //
+
+
+        }
+    }
+}
