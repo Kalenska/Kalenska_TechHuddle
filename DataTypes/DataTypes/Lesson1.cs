8ebe500d82c7586d173ad7192a95e42ad09b2662
Kalenska <julianakalenska@gmail.com>
2020-02-24 17:39:09 +0200

    homework1 - updated

--- DataTypes/DataTypes/Lesson1.cs
+++ DataTypes/DataTypes/Lesson1.cs
@@ -6,49 +6,92 @@ namespace Homework
     {
         static void Main(string[] args)
         {
-            string firstName = "Ivan";
-            string lastName = "Petkov";
-            string city = "Sofia";
-            string street = "Bul. Bulgaria";
-            string houseNumber = "N5";
+            string firstName;
+            string lastName;
+            string city;
+            string street;
+            string houseNumber;
+            string age;
+            string university;
 
-            int age = 20;
-            string university = "Sofia University";
+            int a;
+            int b;
+            int r;
 
-            int a = 3;
-            int b = 2;
-            int r = 5;
 
             //Problem 1 
-            Console.WriteLine("Name: " + firstName + " " + lastName + ", " + "Address: " + city + ", " + street + " " + houseNumber);
+            Console.WriteLine("What is your first name?");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Where city are you from?");
+            city = Console.ReadLine();
+
+            Console.WriteLine("Enter your street?");
+            street = Console.ReadLine();
+
+            Console.WriteLine("Which is your house number");
+            houseNumber = Console.ReadLine();
+
+            string response = ("Your Name is " + firstName + " " + lastName + " and your address is " + city + ", " + street + " " + houseNumber);
+            Console.WriteLine(response);
 
             //Problem 2
-            Console.WriteLine("Name: " + firstName + " " + lastName + ", " + "Age: " + age);
-            Console.WriteLine("University: " + university);
+            Console.WriteLine("What is your first name?");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("What is your last name?");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("How old are you?");
+            age = Console.ReadLine();
+
+            Console.WriteLine("Enter your university?");
+            university = Console.ReadLine();
+
+            string response1 = ("Your Name is " + firstName + " " + lastName + " Age: " + age);
+            string response2 = ("University: " + university);
+            Console.WriteLine(response1);
+            Console.WriteLine(response2);
 
             //Calculations: The first line contains the sum of the two numbers.
             int sum = a + b;
             Console.WriteLine(sum);
 
             //Calculations: The second line contains the difference of the two numbers (first - second).
-            int modulus = a % b;
+            int modulus = a - b;
             Console.WriteLine(modulus);
 
             //Calculations: The third line contains the product of the two numbers.
             int product = a * b;
             Console.WriteLine(product);
 
-            //Operations 1 - square root (solution1) 
-            double sqRoot = Math.Pow(a, 2) + 2 * a * b + Math.Pow(b, 2);
-            Console.WriteLine(sqRoot);
+            //operations 1
+            double result1 = Math.Pow(a, 2) + 2 * a * b + Math.Pow(b, 2);
+            Console.WriteLine(result1);
 
-            //Operations 1 - square root (solution2) 
-            double sqRoot1 = Math.Pow(a + b, 2);
-            Console.WriteLine(sqRoot1);
+            //Operations 2
+            double rToPower2 = Math.Pow(r, 2);
+            double result2 = Math.PI * rToPower2;
+            Console.WriteLine(result2);
 
-            //Operations 2 - Pi; r=5
-            double sumTotal = Math.Pow(Math.PI * r, 2);
-            Console.WriteLine(sumTotal);
+
+            //Operations 3 - T or F 
+            Console.WriteLine(a == 3);
+            Console.WriteLine(a != 3);
+
+            //Operations 4
+            double result2 = Math.Pow(x, y);
+            Console.WriteLine(result2);
+
+            //Operations 5 
+            int area = (height * side) / 2;
+            Console.WriteLine(area);
+
+
+
 
 
 
